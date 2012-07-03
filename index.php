2ee75c896a8650a9ebf4365688abe58d1a510fd8
BeMoreDifferent <albrecht.daniel1990@gmail.com>
2012-07-04 00:24:21 +0200

    removed stuff

--- index.php
+++ index.php
@@ -13,27 +13,12 @@ define('TEST', 'true');
 
 $DB = new SQL();
 
-/*
-$DB->sLastQuery = "CREATE TABLE test (id INTEGER AUTO_INCREMENT,title VARCHAR(255) NOT NULL,text TEXT NOT NULL)";
-$DB->sDBLink->query($DB->sLastQuery);
-*/
+
 //$DB->insert('test', array('title' => 'PDO', 'text' =>' sqlite rockt!!!!! "); die();') );
 //$DB->delete('test',array('title'=>'PDO ist ganz toll'));
-
 //$DB->update('test',array('title'=>'PDO'),array('title'=> 'PDO rockt'));
-
-$insert = array(
-
-		'id'=>9,
-		'title'=>'fooobar 4',
-		'text'=>'sinnloser text ohne sinn'
-
-);
-
-
 //$row = $DB->query("SELECT * FROM test WHERE id=6")->output();
 
-//$DB->insert('test',$insert);
 $row =  $DB->get('test');
 
 //$row = $DB->select('*')->from('test')->where('id = 6',true);
